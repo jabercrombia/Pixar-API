86d3c5551a3bac67db3ef90f7c7cf7c01e681a63
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-03-17 23:48:55 -0500

    favicon

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI
 import pandas as pd
 import io
 import logging
 from db import get_db_connection
 from routes import movie
 from routes import films
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.include_router(movie.router, prefix="/api", tags=["movie"])
 app.include_router(films.router, prefix="/api", tags=["films"])
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return RedirectResponse(url="/static/favicon.ico")
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
